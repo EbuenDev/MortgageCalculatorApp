483aa08a79fcdc1bf119eca4d3822c5b5f59dac9
Devian <markianebuen@gmail.com>
2025-06-22 18:28:56 +0800

    First commit - committing all files to the repository

---
+++ app/src/main/java/com/devian/homepayplanner/MainActivity.java
@@ -0,0 +1,35 @@
+package com.devian.homepayplanner;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewbinding.ViewBinding;
+
+import com.devian.homepayplanner.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+
+        });
+    }
+}
