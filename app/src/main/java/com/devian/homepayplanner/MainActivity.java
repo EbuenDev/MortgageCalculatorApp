fa086e259b23d661439fa7ac645e1cb78e7d3bec
Devian <markianebuen@gmail.com>
2025-06-25 04:03:03 +0800

    fixed the numberException error in main

--- app/src/main/java/com/devian/homepayplanner/MainActivity.java
+++ app/src/main/java/com/devian/homepayplanner/MainActivity.java
@@ -40,67 +40,120 @@ public class MainActivity extends AppCompatActivity {
 
 
         calculateButton.setOnClickListener(v -> {
-            Toast.makeText(this, "Calculating...", Toast.LENGTH_SHORT).show();
-            try{
-                String principalString = binding.principalEditText.getEditText().getText().toString().trim(); //getting the string value from textInputLayout
-                double principal =Double.parseDouble(principalString); //converting the string to double
 
-                String annualInterestString = binding.annualInterestEditText.getEditText().getText().toString().trim();
-                double annualInterest = Double.parseDouble(annualInterestString);
 
-                String loanTermString = binding.loanTermInYearsEditText.getEditText().getText().toString().trim();
-                int loanTerm = Integer.parseInt(loanTermString);
+            String principalString = binding.principalEditText.getEditText().getText().toString().trim(); //getting the string value from textInputLayout
 
-                if(principalString.isEmpty() || annualInterestString.isEmpty() || loanTermString.isEmpty()){
-                    Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
-                    return;
+
+            String annualInterestString = binding.annualInterestEditText.getEditText().getText().toString().trim();
+
+
+            String loanTermString = binding.loanTermInYearsEditText.getEditText().getText().toString().trim();
+
+
+
+            boolean isValid = true;
+
+
+                if (principalString.isEmpty()) {
+                    binding.principalEditText.setError("Principal is required");
+                    binding.principalEditText.requestFocus();
+                    isValid = false;
+
+                    binding.principalEditText.getEditText().setOnFocusChangeListener((view, hasFocus) -> {
+                        if (hasFocus) {
+                            binding.principalEditText.setError(null); // Clear error
+                        }
+                    });
                 }
-                    double monthlyPayment;
-                    double monthlyInterestRate = annualInterest / 12 / 100;
-                    int numberOfPayments = loanTerm * 12;
-
-                    double powerOfNumber = Math.pow(1 + monthlyInterestRate, numberOfPayments); // output is a double
-                    double roundedPowerOfNumber = Math.round(powerOfNumber*1000)/1000.0; // rounding to 3 decimal places
-
-                    //calculating numerator and round to 5 decimal places
-                    double numerator = monthlyInterestRate * roundedPowerOfNumber;
-                    double roundedNumerator = Math.round(numerator*100000)/100000.0; // rounding to 5 decimal places
-
-                    //calculating denominator and round to 5 decimal places
-                    double denominator = roundedPowerOfNumber - 1;
-
-                    //calculate ng fraction to 6 decimal places
-                    double Fraction = roundedNumerator / denominator;
-                    double roundedFraction = Math.round(Fraction*1000000)/1000000.0; // rounding to 6 decimal places
-
-                    monthlyPayment = principal * roundedFraction;
-
-                    //getting the default locale for Philippines
-                    Locale philippines = new Locale("en", "PH");
-                    NumberFormat currency = NumberFormat.getCurrencyInstance(philippines);
-                    NumberFormat currencyMonthly = NumberFormat.getCurrencyInstance(philippines);
-                    String monthlyPaymentString = currencyMonthly.format(monthlyPayment); //passing the monthlyPayment int to string for display
-                    currency.setMaximumFractionDigits(0);
-                    String formattedValue = currency.format(principal); //"₱ "
-                    String viewInterest = String.valueOf(annualInterest);
-                    String viewLoanTerm = String.valueOf(loanTerm);
-
-                    //Fomatting the Principal in every 3 digits to have commas
-
-                    DecimalFormat decimalFormat = new DecimalFormat("#,###");
-
-                    //Displaying the Calculated Result passing all the value in textview
-                    Log.d("DEBUG", "Formatted Principal: " + formattedValue);
-                    binding.principalTextView.setText(formattedValue);
-                    binding.interestRateTextView.setText(viewInterest + " %");
-                    binding.periodOfYearsTextView.setText(viewLoanTerm + " Years");
-                    Log.d("DEBUG", "Monthly Payment: " + monthlyPaymentString);
-                    binding.monthlyPaymentTextView.setText(String.valueOf(monthlyPaymentString));
 
 
-            } catch (NumberFormatException e) {
-                Toast.makeText(this, "Input only numberical values", Toast.LENGTH_SHORT).show();
-            }
+
+                if (annualInterestString.isEmpty()) {
+                    binding.annualInterestEditText.setError("Annual interest is required");
+                    binding.annualInterestEditText.requestFocus();
+                    isValid = false;
+
+                    binding.annualInterestEditText.getEditText().setOnFocusChangeListener((view, hasFocus) -> {
+                        if (hasFocus) {
+                            binding.annualInterestEditText.setError(null); // Clear error
+                        }
+                    });
+                }
+
+                if (loanTermString.isEmpty()) {
+                    binding.loanTermInYearsEditText.setError("Loan term is required");
+                    binding.loanTermInYearsEditText.requestFocus();
+                    isValid = false;
+
+                    binding.loanTermInYearsEditText.getEditText().setOnFocusChangeListener((view, hasFocus) -> {
+                        if (hasFocus) {
+                            binding.loanTermInYearsEditText.setError(null); // Clear error
+                        }
+                    });
+                }
+
+                if (isValid) {
+                    try{
+
+                        double principal =Double.parseDouble(principalString); //converting the string to double
+                        double annualInterest = Double.parseDouble(annualInterestString);
+                        int loanTerm = Integer.parseInt(loanTermString);
+
+                        Toast.makeText(this, "Calculating...", Toast.LENGTH_SHORT).show();
+                        double monthlyPayment;
+                        double monthlyInterestRate = annualInterest / 12 / 100;
+                        int numberOfPayments = loanTerm * 12;
+
+                        double powerOfNumber = Math.pow(1 + monthlyInterestRate, numberOfPayments); // output is a double
+                        double roundedPowerOfNumber = Math.round(powerOfNumber*1000)/1000.0; // rounding to 3 decimal places
+
+                        //calculating numerator and round to 5 decimal places
+                        double numerator = monthlyInterestRate * roundedPowerOfNumber;
+                        double roundedNumerator = Math.round(numerator*100000)/100000.0; // rounding to 5 decimal places
+
+                        //calculating denominator and round to 5 decimal places
+                        double denominator = roundedPowerOfNumber - 1;
+
+                        //calculate ng fraction to 6 decimal places
+                        double Fraction = roundedNumerator / denominator;
+                        double roundedFraction = Math.round(Fraction*1000000)/1000000.0; // rounding to 6 decimal places
+
+                        monthlyPayment = principal * roundedFraction;
+
+                        //getting the default locale for Philippines
+                        Locale philippines = new Locale("en", "PH");
+                        NumberFormat currencyMonthly = NumberFormat.getCurrencyInstance(philippines);
+                        String monthlyPaymentString = currencyMonthly.format(monthlyPayment); //passing the monthlyPayment int to string for display
+
+                        NumberFormat currency = NumberFormat.getCurrencyInstance(philippines);
+                        currency.setMaximumFractionDigits(0);
+                        String formattedValue = currency.format(principal);
+
+                        NumberFormat percentage = NumberFormat.getPercentInstance();
+                        percentage.setMinimumFractionDigits(1);
+                        String formattedInterest = percentage.format(annualInterest/100);
+
+                        String viewLoanTerm = String.valueOf(loanTerm);
+
+                        //Fomatting the Principal in every 3 digits to have commas
+
+                        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+
+                        //Displaying the Calculated Result passing all the value in textview
+                        Log.d("DEBUG", "Formatted Principal: " + formattedValue);
+                        binding.principalTextView.setText(formattedValue);
+                        binding.interestRateTextView.setText(formattedInterest);
+                        binding.periodOfYearsTextView.setText(viewLoanTerm);
+                        Log.d("DEBUG", "Monthly Payment: " + monthlyPaymentString);
+                        binding.monthlyPaymentTextView.setText(String.valueOf(monthlyPaymentString));
+
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(this, "Input only numberical values", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
 
         });
 
